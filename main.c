4a4b6e1ec003bbfba07b30b1b05d10ce2bb451fb
EduardoHaagP <163572315+EduardoHaagP@users.noreply.github.com>
2024-12-04 19:04:48 -0300

    menu
    Menu básico

---
+++ main.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+//enum para facilitar manutenção cogido menu
+enum Opcoes{SAIR = 0, JOGAR, DECKS};
+
+void jogar(){
+    printf("Em Breve\n");
+    return;
+}
+
+void decks(){
+    printf("Em Breve\n");
+    return;
+}
+
+void menu(int *vmenu){
+    printf("1 - Jogar\n2 - Decks\n0 - Sair");
+    scanf("%i",&vmenu);
+    return;
+}
+
+int main()
+{
+    int vmenu = -1;
+    do
+    {
+        menu(&vmenu);
+        switch (vmenu)
+    {
+    case JOGAR:
+        jogar();
+        break;
+    case DECKS:
+        decks();
+        break;
+    
+    default:
+        printf("O valor digitado não é valido\n");
+        break;
+    }
+    } while (vmenu != SAIR);
+    return 0;
+}
