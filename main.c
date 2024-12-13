6c6381701a8e3e974f1babcbe4733283be5b5629
EduardoHaagP <163572315+EduardoHaagP@users.noreply.github.com>
2024-12-13 20:36:47 -0300

    alterar

--- main.c
+++ main.c
@@ -4,6 +4,7 @@
 #include <locale.h>
 
 // Enum para facilitar manutenção do menu Principal
+// Enum para facilitar manutenção do menu Deck
 enum Opcoes
 {
     SAIR = 0,
@@ -13,16 +14,22 @@ enum Opcoes
     LISTAR,
     PESQUISAR,
     ALTERAR,
-    EXCLUIR
+    EXCLUIR,
+    NOME = 1,
+    LETRA,
+    SUPERTRUNFO,
+    ANOCONSTRUCAO,
+    ALTURA,
+    VISITASANUAIS,
+    IMPORTANCIAHISTORICA,
+    POPULARIDADE
 };
-
-// Enum para facilitar manutenção do menu Deck
 
 typedef struct
 {
     char nome[51];
     char letra;
-    int supertrunfo; // Agora é booleano
+    int supertrunfo;
     int anoConstrucao;
     float altura;
     int visitasAnuais;
@@ -77,18 +84,20 @@ void inserirCartas(cartas **deck, int *quantidadeCartas)
         setbuf(stdin, NULL);
         if (respostaLetra == 1)
         {
-            carta.letra == 'A';
-        }else if (respostaLetra == 2)
+            carta.letra = 'A';
+        }
+        else if (respostaLetra == 2)
         {
-            carta.letra == 'B';
-        }else if (respostaLetra == 3)
+            carta.letra = 'B';
+        }
+        else if (respostaLetra == 3)
         {
-            carta.letra == 'C';
-        }else if (respostaLetra == 4)
+            carta.letra = 'C';
+        }
+        else if (respostaLetra == 4)
         {
-            carta.letra == 'D';
-        } 
-        
+            carta.letra = 'D';
+        }
 
         int resposta;
         printf("Deseja que ele se torne o super trunfo\n1 - sim\n0 - nao\nDigite aqui: ");
@@ -186,7 +195,7 @@ void inserirCartas(cartas **deck, int *quantidadeCartas)
 
 void listarCartas(cartas **deck, int *quantidadeCartas)
 {
-    printf("%6s - %35s| %5c| %5s| %12s| %14s| %15s| %20s| %14s\n", "Indice", "Nome do Monumento", "Letra", "Super Trunfo", "Ano Construcao", "Altura","Visitas Anuais", "Importancia Historica", "Popularidade");
+    printf("%6s - %35s| %7s| %5s| %12s| %14s| %15s| %20s| %14s\n", "Indice", "Nome do Monumento", "Letra", "Super Trunfo", "Ano Construcao", "Altura", "Visitas Anuais", "Importancia Historica", "Popularidade");
     for (int i = 0; i < (*quantidadeCartas); i++)
     {
         if ((*deck)[i].supertrunfo == 1)
@@ -243,10 +252,167 @@ void pesquisarCartas(cartas **deck, int *quantidadeCartas)
     return;
 }
 
-void alterarCartas()
+void alterarCartas(cartas **deck, int *quantidadeCartas)
 {
-    printf("Em breve.\n");
-    return;
+    int indice;
+    printf("Qual o Indice da carta que voce deseja alterar (0 - 31): ");
+    scanf("%i", &indice);
+    setbuf(stdin, NULL);
+    while (indice > 31 && indice < 0)
+    {
+        printf("O indice digitado nao corresponde a nenhuma carta!\n Digite novamente: ");
+        scanf("%i", &indice);
+        setbuf(stdin, NULL);
+    }
+    printf("%6s - %35s| %7s| %14s| %16s| %12s| %15s| %22s| %14s\n", "Indice", "Nome do Monumento", "Letra", "Super Trunfo", "Ano Construcao", "Altura", "Visitas Anuais", "Importancia Historica", "Popularidade");
+    if ((*deck)[indice].supertrunfo == 1)
+    {
+        printf("%6i - %35s| %7c| %14s| %16i| %12.2f| %15i| %22i| %14i\n", indice, (*deck)[indice].nome, (*deck)[indice].letra, "Sim",
+               (*deck)[indice].anoConstrucao, (*deck)[indice].altura,
+               (*deck)[indice].visitasAnuais, (*deck)[indice].importanciaHistorica,
+               (*deck)[indice].popularidade);
+    }
+    else if ((*deck)[indice].supertrunfo == 0)
+    {
+        printf("%6i - %35s| %7c| %14s| %16i| %12.2f| %15i| %22i| %14i\n", indice, (*deck)[indice].nome, (*deck)[indice].letra, "Nao",
+               (*deck)[indice].anoConstrucao, (*deck)[indice].altura,
+               (*deck)[indice].visitasAnuais, (*deck)[indice].importanciaHistorica,
+               (*deck)[indice].popularidade);
+    }
+    int resposta;
+    printf("E essa carta que voce deseja alterar?\n1-Sim\n2-nao\nDigite aqui: ");
+    scanf("%i", &resposta);
+    setbuf(stdin, NULL);
+    while (resposta != 1 && resposta != 0)
+    {
+        printf("Resposta invalida\nDigite novamente: ");
+        scanf("%i", &resposta);
+        setbuf(stdin, NULL);
+    }
+    if (resposta == 0)
+    {
+        alterarCartas(deck, quantidadeCartas);
+    }
+    else
+    {
+        int respostaAlterarcarta = -1;
+        printf("O que deseja alterar nessa carta\n1-Nome\n2-Letra\n3-Super trunfo\n4-Ano Construcao\n5- Altura\n6-Visitas Anuais\n7-Importancia Historica\n8-Popularidade\nDigite aqui: ");
+        scanf("%i", &respostaAlterarcarta);
+        setbuf(stdin, NULL);
+        while (respostaAlterarcarta > 8 || respostaAlterarcarta < 1)
+        {
+            printf("Resposta invalida\nDigite novamente: ");
+            scanf("%i", &respostaAlterarcarta);
+        }
+        int respostasAux;
+        switch (respostaAlterarcarta)
+        {
+        case NOME:
+            printf("Qual o nome que deseja atribuir a essa carta: ");
+            fgets((*deck)[indice].nome, sizeof((*deck)[indice]), stdin);
+            (*deck)[indice].nome[strcspn((*deck)[indice].nome, "\n")] = '\0';
+            setbuf(stdin, NULL);
+            break;
+        case LETRA:
+            printf("Qual letra voce deseja que essa carta possua:1 - A\n2 - B\n3 - C\n4 - D\nDigite aqui: ");
+            scanf("%i", &respostasAux);
+            setbuf(stdin, NULL);
+            if (respostasAux == 1)
+            {
+                (*deck)[indice].letra = 'A';
+            }
+            else if (respostasAux == 2)
+            {
+                (*deck)[indice].letra = 'B';
+            }
+            else if (respostasAux == 3)
+            {
+                (*deck)[indice].letra = 'C';
+            }
+            else if (respostasAux == 4)
+            {
+                (*deck)[indice].letra = 'D';
+            }
+            break;
+        case SUPERTRUNFO:
+            printf("Deseja que ele se torne o super trunfo\n(Somente pode existir 1 Super Trunfo! Caso escolha sim o outro existente deixara de ser)\n1 - sim\n0 - nao\nDigite aqui: ");
+            scanf("%i", &respostasAux);
+            setbuf(stdin, NULL);
+            while (respostasAux != 1 && respostasAux != 0)
+            {
+                printf("Resposta invalida!\nDigite novamente: ");
+                scanf("%i", &respostasAux);
+                setbuf(stdin, NULL);
+            }
+
+            if (respostasAux == 1)
+            {
+                // Desmarca todos os super trunfos no deck
+                for (int i = 0; i < (*quantidadeCartas); i++)
+                {
+                    (*deck)[i].supertrunfo = 0;
+                }
+                (*deck)[indice].supertrunfo = 1;
+            }
+            else
+            {
+                (*deck)[indice].supertrunfo = 0;
+            }
+            break;
+        case ANOCONSTRUCAO:
+            printf("Qual o ano de termino da construcao deseja atribuir a essa carta (em caso de ser antes de cristo digite o valor negativo): ");
+            scanf("%i", &(*deck)[indice].anoConstrucao);
+            setbuf(stdin, NULL);
+
+            break;
+        case ALTURA:
+            printf("Qual a altura desse monumento: ");
+            scanf("%f", &(*deck)[indice].altura);
+            setbuf(stdin, NULL);
+            break;
+        case VISITASANUAIS:
+            printf("Quantas visitas anuais esse monumento recebe em media: ");
+            scanf("%i", &(*deck)[indice].visitasAnuais);
+            setbuf(stdin, NULL);
+            break;
+        case IMPORTANCIAHISTORICA:
+            printf("De 0 a 10 qual a sua importancia historica: ");
+            scanf("%i", &respostasAux);
+            setbuf(stdin, NULL);
+            while (respostasAux > 10 || respostasAux < 0)
+            {
+                printf("Resposta invalida!\nDigite novamente: ");
+                scanf("%i", &respostasAux);
+                setbuf(stdin, NULL);
+            }
+            (*deck)[indice].importanciaHistorica = respostasAux;
+            break;
+        case POPULARIDADE:
+            printf("De 0 a 10 qual a sua popularidade: ");
+            scanf("%i", &respostasAux);
+            setbuf(stdin, NULL);
+            while (respostasAux > 10 || respostasAux < 0)
+            {
+                printf("Resposta invalida!\nDigite novamente: ");
+                scanf("%i", &respostasAux);
+                setbuf(stdin, NULL);
+            }
+            (*deck)[indice].popularidade = respostasAux;
+            break;
+        }
+        FILE *arq = fopen("deck.csv", "w");
+        if (arq != NULL)
+        {
+            for (int i = 0; i < (*quantidadeCartas); i++)
+            {
+                fprintf(arq, "%s,%c,%i,%i,%.2f,%i,%i,%i\n", (*deck)[i].nome, (*deck)[i].letra, (*deck)[i].supertrunfo,
+                        (*deck)[i].anoConstrucao, (*deck)[i].altura, (*deck)[i].visitasAnuais, (*deck)[i].importanciaHistorica,
+                        (*deck)[i].popularidade);
+            }
+            fclose(arq);
+        }
+        return;
+    }
 }
 
 void excluirCartas()
@@ -275,7 +441,7 @@ void menuDecks(cartas **deck, int *quantidadeCartas)
             pesquisarCartas(deck, quantidadeCartas);
             break;
         case ALTERAR:
-            alterarCartas();
+            alterarCartas(deck, quantidadeCartas);
             break;
         case EXCLUIR:
             excluirCartas();
@@ -302,7 +468,7 @@ void menu(int *vmenu)
 cartas configuracoesIniciais(FILE *arq)
 {
     cartas deck;
-    fscanf(arq, "%[^,],%c,%i,%i,%f,%i,%i,%i\n", deck.nome, deck.letra, &deck.supertrunfo, &deck.anoConstrucao, &deck.altura, &deck.visitasAnuais, &deck.importanciaHistorica, &deck.popularidade);
+    fscanf(arq, "%[^,],%c,%i,%i,%f,%i,%i,%i\n", deck.nome, &deck.letra, &deck.supertrunfo, &deck.anoConstrucao, &deck.altura, &deck.visitasAnuais, &deck.importanciaHistorica, &deck.popularidade);
     if (deck.supertrunfo != 1 && deck.supertrunfo != 0)
     {
         deck.supertrunfo = 0;
