62c6dd815b37c8e5cd48fa57e6279b4d924c4a55
EduardoHaagP <163572315+EduardoHaagP@users.noreply.github.com>
2025-01-22 13:56:06 -0300

    fix

--- projeto/src/funcoes.h
+++ projeto/src/funcoes.h
@@ -4,9 +4,6 @@
 #include "cartas.h" // Inclui a definição da estrutura cartas
 #include "jogar.h"
 
-void jogar();
-void menu(int *vmenu);
-void menuDecks(cartas **deck, int *quantidadeCartas);
 int contarcartas(FILE *arq);
 cartas configuracoesIniciais(FILE *arq);
 cartas *carregarDeck(const char *nomeArquivo, int *quantidadeCartas);
